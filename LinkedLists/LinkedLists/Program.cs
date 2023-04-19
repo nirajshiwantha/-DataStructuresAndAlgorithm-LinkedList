8ade2fa4e548af0264184c4885df1640b7a1730d
niraj_ranasinghe <niraj.ranasinghe@covalentworld.com>
2023-04-19 07:55:26 +0530

    Initial Commit with Linear Traversal

---
+++ LinkedLists/LinkedLists/Program.cs
@@ -0,0 +1,31 @@
+﻿using LinkedLists.TraversalAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new linked list
+            LinkedList myList = new LinkedList();
+
+            // add some elements to the linked list
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(8);
+            myList.Add(5);
+
+            // perform linear traversal of the linked list
+            Console.WriteLine("Performing linear traversal...");
+            myList.LinearTraversal();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinkedLists/LinkedLists/TraversalAlgorithms/LinearTraversal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedLists.TraversalAlgorithms
+{
+    public class LinkedList
+    {
+        // define the Node class
+        public class Node
+        {
+            public int Data;
+            public Node Next;
+
+            public Node(int data)
+            {
+                this.Data = data;
+                this.Next = null;
+            }
+        }
+
+        // define the head of the linked list
+        private Node Head;
+
+        // define the constructor for the linked list
+        public LinkedList()
+        {
+            this.Head = null;
+        }
+
+        // define the method to add a new node to the linked list
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (this.Head == null)
+            {
+                this.Head = newNode;
+            }
+            else
+            {
+                Node currentNode = this.Head;
+
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                currentNode.Next = newNode;
+            }
+        }
+
+        // define the method to perform linear traversal of the linked list
+        public void LinearTraversal()
+        {
+            Node currentNode = this.Head;
+
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Data);
+                currentNode = currentNode.Next;
+            }
+        }
+    }
+}
